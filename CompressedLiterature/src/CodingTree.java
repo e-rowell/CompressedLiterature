223c713570432a7bbe414cbc812a8e3f50ab9d5f
nicholas hays <nicholas.hays@hotmail.com>
2016-01-31 00:33:02 -0800

    first commit

---
+++ CompressedLiterature/src/CodingTree.java
@@ -0,0 +1,27 @@
+import java.util.List;
+import java.util.Map;
+//
+public class CodingTree {
+	
+	// map of characters in the message to binary codes
+	public Map<Character, String> codes;
+	// message encoded using the Huffman codes.
+	List<Byte> bits;
+		/*
+		 * a constructor that takes the text of a message to be
+		 * compressed. The constructor is responsible for calling
+		 * all private methods that carry out the Huffman coding algorithm.
+		 */
+	public CodingTree(String message) {
+		
+		
+	}
+	
+	//(Optional)​String decode(String bits, Map<Character, String> codes)
+	
+	
+	
+	
+	
+	
+}

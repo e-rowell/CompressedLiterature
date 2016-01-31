f240b6680e8275721b592aa5bd3dc04f7aef9ec3
nicholas hays <nicholas.hays@hotmail.com>
2016-01-31 02:39:03 -0800

    Defined CodingTree Class & Main InputStreamReader
    parseChars(String)
    createCodes();

--- CompressedLiterature/src/CodingTree.java
+++ CompressedLiterature/src/CodingTree.java
@@ -1,27 +1,87 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.PriorityQueue;
+
 //
 public class CodingTree {
-	
+
 	// map of characters in the message to binary codes
 	public Map<Character, String> codes;
+	public Map<Character, Integer> charFreq;
+	PriorityQueue<TreeNode<Character>> pq;
 	// message encoded using the Huffman codes.
 	List<Byte> bits;
-		/*
-		 * a constructor that takes the text of a message to be
-		 * compressed. The constructor is responsible for calling
-		 * all private methods that carry out the Huffman coding algorithm.
-		 */
+
+	/*
+	 * a constructor that takes the text of a message to be compressed. The
+	 * constructor is responsible for calling all private methods that carry out
+	 * the Huffman coding algorithm.
+	 */
 	public CodingTree(String message) {
-		
-		
+		codes = new HashMap<>();
+		charFreq = new HashMap<>();
+		pq = new PriorityQueue<>();
+		parseChars(message);
+		createCodes();
 	}
+
+	private void parseChars(String message) {
+		for (char c : message.toCharArray()) {
+			if (charFreq.get(c) == null) {
+				charFreq.put(c, 1);
+			} else {
+				charFreq.put(c, charFreq.get(c).intValue() + 1);
+			}
+		}
+
+	}
+
+	private void createCodes() {
+		for (Character c : charFreq.keySet()) {
+			TreeNode<Character> treeNode = new TreeNode<>();
+			treeNode.character = c;
+			treeNode.weight = charFreq.get(c);
+			pq.add(treeNode);
+		}
+		ArrayList<TreeNode<?>> nodes = new ArrayList<TreeNode<?>>();
+		
+		Object[] objArray = pq.toArray();
+		for(int i = 0; i < objArray.length; i++) {
+			Object object = objArray[i];
+			if (object instanceof TreeNode<?>) {
+				nodes.add((TreeNode<?>) object);
+			}
+		}
+		Arrays.sort(nodes.toArray());
+	}
+		
 	
-	//(Optional)​String decode(String bits, Map<Character, String> codes)
-	
-	
-	
-	
-	
-	
+	// (Optional)​String decode(String bits, Map<Character, String> codes)
+
+	private class TreeNode<T> implements Comparable<T> {
+		TreeNode<T> root;
+		TreeNode<T> left;
+		TreeNode<T> right;
+		T character;
+		Integer weight;
+
+		public TreeNode() {
+
+		}
+
+		@Override
+		public int compareTo(T o) {
+			Integer otherInt = (Integer) o;
+			if (this.weight < otherInt)
+				return -1;
+			else if (this.weight == otherInt)
+				return 0;
+			else
+				return 1;
+		}
+	}
+
 }
--- CompressedLiterature/src/Main.java
+++ CompressedLiterature/src/Main.java
@@ -1,9 +1,33 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		BufferedReader reader = null;
+		StringBuilder str = new StringBuilder();
+		try {
+			reader = new BufferedReader(new FileReader("WarAndPeace.txt"));
+			while (reader.ready()) {
+				str.append(reader.readLine() + System.lineSeparator());
+			}
+			reader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		BufferedWriter bw;
+		try {
+			bw = new BufferedWriter(new FileWriter("test.txt"));
+			bw.write(str.toString());
+			bw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		CodingTree tree = new CodingTree(str.toString());
 	}
 
 }
